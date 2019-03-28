2a9d7a53226984d5f663525cc40d2358b92c7cb7
Jake Streett <jakestreett@gmail.com>
2019-03-28 15:27:10 -0600

    backend update, added post and delete

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI : 'mongodb://jake:jake123@cluster0-shard-00-00-pmqcq.mongodb.net:27017,cluster0-shard-00-01-pmqcq.mongodb.net:27017,cluster0-shard-00-02-pmqcq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+    mongoURI : 'mongodb://jake:jake123@cluster0-shard-00-00-pmqcq.mongodb.net:27017,cluster0-shard-00-01-pmqcq.mongodb.net:27017,cluster0-shard-00-02-pmqcq.mongodb.net:27017/ShoppingListDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 }
--- routes/api/items.js
+++ routes/api/items.js
@@ -12,9 +12,32 @@ const Item = require('../../models/Items');
 router.get('/', (req, res) =>{
     Item.find()
     .sort({date:-1})
-    .then(items => res.jason(items))
-});
+    .then(items => res.json(items))
+}) ;
 
+// @route POST api/items
+// @desc Create A Item
+// @access Public 
+// just the / because it is defined in the server.js to always go to
+// ... app.use('/api/items', items)
+router.post('/', (req, res) =>{
+    const newItem = new Item({
+        name: req.body.name
+    })
+
+    newItem.save().then(item => res.json(item));
+}) ;
+
+// @route DELETE api/items:id
+// @desc Delete A Item
+// @access Public 
+// just the / because it is defined in the server.js to always go to
+// ... app.use('/api/items', items)
+router.delete('/:id', (req, res) =>{
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(()=> res.json({success:true, body: "Deleted item: " + item} )))
+    .catch(err => res.status(404).json({sucess: false}))
+});
 
 
 module.exports = router;
